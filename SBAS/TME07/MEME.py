53f9958c45f54ac3481891b4ff5a37209b81771d
WANG Jinxin <wangjinxin.fr@gmail.com>
2015-04-29 11:47:37 +0200

    SBAS TME 07

--- SBAS/TME07/MEME.py
+++ SBAS/TME07/MEME.py
@@ -1,31 +1,102 @@
 #!env python
 
-import pylab
-import pickle
-import operator
-import subprocess
 import numpy as np
-import os.path as op
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
 from Bio import SeqIO
-from Bio import SearchIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio.Alphabet import generic_dna
-from Bio.SeqIO.FastaIO import FastaWriter
-from Bio.Align.Applications import ClustalwCommandline
 
-def readFasta(fb):
-    seqDict = {}
-    for record in SeqIO.parse(fb, "fasta") :
-        desc = record.description
-        record.desc = descriptionDict(desc)
-        if not seqDict.has_key(record.id):
-            seqDict[record.id] = record
-        else:
-            raise ValueError("Duplicated Sequence ID [%d] in fasta. "%(record.id))
-    return seqDict
+code_dict = {'A':0,'C':1,'G':2,'T':3,
+             'a':0,'c':1,'g':2,'t':3}
 
+def readFasta(handle):
+    seqList = []
+    for record in SeqIO.parse(handle, "fasta") :
+        seqList.append(record)
+    return seqList
+
+def P(seq,pos,pt,W):
+    prob = 1
+    for p in range(0,pos):
+        prob *= pt[code_dict[seq[p]],0]
+    for p in range(pos,pos+W):
+        prob *= pt[code_dict[seq[p]],p-pos+1]
+    for p in range(pos+W,len(seq)):
+        prob *= pt[code_dict[seq[p]],0]
+    return prob
+        
+def likelihood_log(seqList,Z,pt,W):
+    ll = 0
+    for i,record in enumerate(seqList):
+        pos = np.argmax(Z[i])
+        seq = record.seq
+        ll += np.log(P(seq,pos,pt,W)/(len(seq)-W+1))
+    return ll
+
+def init_p0_random(seqList,W): 
+    p0 = np.ones((4,W+1)) / 6
+    p0[:,0] = np.ones(4)*0.25
+    i = np.random.randint(len(seqList))
+    j = np.random.randint(len(seqList[i].seq)-W)
+    # print seqList[i].seq[j:j+W]
+    for indice,c in enumerate(seqList[i].seq[j:j+W]):
+        p0[code_dict[c],indice+1] = 0.5
+    return p0
+
+def E_step(seqList,pt,W):
+    Zt = np.array([[ P(record.seq,j,pt,W) for j in range(len(record.seq)-W+1) ] for i,record in enumerate(seqList) ])
+    # normalisation de Zt
+    for i,s in enumerate(Zt.sum(axis=1)):
+        Zt[i] = Zt[i]/s
+    return Zt
+
+def Nc(seqList,c):
+    seq = ''.join([ "%s"%record.seq for record in seqList ])
+    return sum(np.array(list(seq)) == c)
+
+def M_step(seqList,W,Zt):
+    # Calcul de Pck
+    pt = np.ones((4,W+1))
+    for i,record in enumerate(seqList):
+        for pos,c in enumerate(record.seq):
+            L = len(record.seq)
+            if pos + 1 >= W :
+                if pos <= L - W + 1:
+                    jRange = range(0,W)
+                else:
+                    jRange = range(pos+W-L-1,W)
+            else :
+                jRange = range(0,pos+1)
+            for j in jRange:
+                pt[code_dict[c],j+1] += Zt[i,pos-j-1]
+    # Calcul de P0
+    pt[:,0] = [1+Nc(seqList,'A')-pt[0,1:].sum(),
+               1+Nc(seqList,'C')-pt[1,1:].sum(),
+               1+Nc(seqList,'G')-pt[2,1:].sum(),
+               1+Nc(seqList,'T')-pt[3,1:].sum()]
+    # normalisation de Pt
+    pt[:,1:] = pt[:,1:] / (Zt.sum() + 4)
+    # normalisation de P0
+    pt[:,0]  = pt[:,0] / sum(pt[:,0])
+    return pt
+
+def MEME(seqList,W,epsilon):
+    # init p0
+    pt = init_p0_random(seqList,W)
+    print "p0 : \n", pt,"\n"
+    vr_list = []
+    # Iteration until change in p(t) < epsilon
+    while len(vr_list) < 2 or abs(vr_list[-2] - vr_list[-1]) > epsilon :
+        Zt = E_step(seqList,pt,W)
+        pt = M_step(seqList,W,Zt)
+        vr_list.append(likelihood_log(seqList,Zt,pt,W))
+    return pt,Zt
+
+W = 14
+handle = open("hw1_hidden_motif.txt")
+seqList = readFasta(handle)
+# print init_p0_random(seqList,W)
+pt,Zt =  MEME(seqList,W,0.0000000001)
+
+print "pt : \n",pt
+print '\n'
+print "Zt : \n",Zt
+print "motives positions : ", np.argmax(Zt,axis=1)
 
