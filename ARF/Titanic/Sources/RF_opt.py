bd48e96ade4f1cb5a2485d6db648b615a2cc4e55
WANG Jinxin <wangjinxin.java@gmail.com>
2015-05-03 16:47:24 +0200

    ARF Titanic

--- ARF/Titanic/Sources/RF_opt.py
+++ ARF/Titanic/Sources/RF_opt.py
@@ -1,8 +1,9 @@
 #!env python
 
+import pandas as pd
 import numpy as np
 import csv as csv
-from sklearn.grid_search import GridSearchCV
+from sklearn.grid_search import GridSearchCV,RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 
 def report(grid_scores, n_top=5):
@@ -20,8 +21,22 @@ def report(grid_scores, n_top=5):
     
     return params
 
+# Data cleanup
+# TRAIN DATA
+train_df = pd.read_csv('cleanedTrain.csv', header=0)        # Load the train file into a dataframe
+test_df  = pd.read_csv('cleanedTest.csv',  header=0)        # Load the test file into a dataframe
+
+y = train_df.Survived.values
+test_id = test_df.PassengerId.values
+
+# The data is now ready to go. So lets fit to the train, then predict to the test!
+# Convert back to a numpy array
+train_df = train_df.drop(['Survived'],axis=1) 
+X  = train_df.values
+tX = test_df.values
+    
 # The most common value for the max number of features to look at in each split is sqrt(# of features)
-sqrtfeat = np.sqrt(X.shape[1]) 
+sqrtfeat = int(np.sqrt(X.shape[1]))
 
 # Simple grid test (162 combinations)
 grid_test1 = { "n_estimators"      : [1000, 2500, 5000],
@@ -34,9 +49,9 @@ grid_test1 = { "n_estimators"      : [1000, 2500, 5000],
 random_test1 = { "n_estimators"      : np.rint(np.linspace(X.shape[0]*2, X.shape[0]*4, 5)).astype(int),
                  "criterion"         : ["gini", "entropy"],
                  "max_features"      : np.rint(np.linspace(sqrtfeat/2, sqrtfeat*2, 5)).astype(int),
-                 "max_depth"         : np.rint(np.linspace(1, X.shape[1]/2, 10),
+                 "max_depth"         : np.rint(np.linspace(1, X.shape[1]/2, 10)),
                  "min_samples_split" : np.rint(np.linspace(2, X.shape[0]/50, 10)).astype(int) }
-
+                 
 # Large randomized test using min_samples_leaf and max_leaf_nodes to control tree size (50k combinations)
 random_test2 = { "n_estimators"      : np.rint(np.linspace(X.shape[0]*2, X.shape[0]*4, 5)).astype(int),
                  "criterion"         : ["gini", "entropy"],
@@ -46,18 +61,19 @@ random_test2 = { "n_estimators"      : np.rint(np.linspace(X.shape[0]*2, X.shape
                  "max_leaf_nodes"    : np.rint(np.linspace(10, X.shape[0]/50, 10)).astype(int) }
 
 forest = RandomForestClassifier(oob_score=True)
-
+'''
 print "Hyperparameter optimization using GridSearchCV..."
 grid_search = GridSearchCV(forest, grid_test1, n_jobs=-1, cv=10)
 grid_search.fit(X, y)
 best_params_from_grid_search = report(grid_search.grid_scores_)
-
+'''
 print "Hyperparameter optimization using RandomizedSearchCV with max_depth parameter..."
 grid_search = RandomizedSearchCV(forest, random_test1, n_jobs=-1, cv=10, n_iter=100)
 grid_search.fit(X, y)
 best_params_from_rand_search1 = report(grid_search.grid_scores_)
-
+'''
 print "...and using RandomizedSearchCV with min_samples_leaf + max_leaf_nodes parameters..."
 grid_search = RandomizedSearchCV(forest, random_test2, n_jobs=-1, cv=10, n_iter=500)
 grid_search.fit(X, y)
 best_params_from_rand_search2 = report(grid_search.grid_scores_)
+'''
