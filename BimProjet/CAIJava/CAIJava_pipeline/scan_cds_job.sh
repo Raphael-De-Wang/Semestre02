bbcde24ba64c54c7b839741670d7b1e9b2b34f96
WANG Jinxin <wangjinxin.fr@gmail.com>
2015-07-10 17:00:51 +0200

    Bim Projet Summer

--- BimProjet/CAIJava/CAIJava_pipeline/scan_cds_job.sh
+++ BimProjet/CAIJava/CAIJava_pipeline/scan_cds_job.sh
@@ -7,12 +7,12 @@ hmmfile='/users/Etu9/3404759/Workspace/Semestre02/BimProjet/CAIJava/source/Pfam/
 for file in `ls $basepath` ; do
     if [ -d "$basepath$file" ] ; then
 	for sfile in `ls "$basepath$file"` ; do
-	    if [[ $sfile == *"translate.fasta" ]] ; then
+	    if [[ $sfile == *".fsa_nt" ]] ; then
 		# hmmscan --domtblout /tmp/Spirodela_polyrhiza.domtblout Pfam/Pfam-A.hmm Marine/Streptophyta/Spirodela_polyrhiza_translate.fasta
 		fname="$basepath$file/$sfile"
 		# echo "hmmscan --domtblout ${fname/_translate.fasta/.domtblout} ${hmmfile} ${fname} > ${fname/_translate.fasta/_scan.output}"
 		# du -sh $fname
-		echo "~/Workspace/toolkits/FragGeneScan1.19/run_FragGeneScan.pl -genome=${fname} -out=${fname/fasta/out} -complete=0 -train=complete -thread=10"
+		echo "~/Workspace/toolkits/FragGeneScan1.19/run_FragGeneScan.pl -genome=${fname} -out=${fname/fsa_nt/fragGeneScan} -complete=0 -train=complete -thread=10"
 	    fi
 	done
     fi
