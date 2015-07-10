a16f831fa7766091335ac98135e93cd9d91c31a8
WANG Jinxin <wangjinxin.fr@gmail.com>
2015-07-10 15:57:47 +0200

    Bim Projet Summer

--- BimProjet/CAIJava/CAIJava_pipeline/verify_cds_numbers.sh
+++ BimProjet/CAIJava/CAIJava_pipeline/verify_cds_numbers.sh
@@ -5,10 +5,10 @@ path='/users/Etu9/3404759/Workspace/Semestre02/BimProjet/CAIJava/source/Marine/'
 
 for sub in `ls $path`; do
     for ssub in `ls "${path}${sub}"`; do
-	if [[ -d "${path}${sub}" && "${path}${sub}/${ssub}" = *"gb"* ]] ; then
+	if [[ -d "${path}${sub}" && ("${path}${sub}/${ssub}" = *"gb"* ) ]] ; then
 	    count=`grep -n CDS "${path}${sub}/${ssub}" | wc -l`
 	    if [ "$count" != "0" ]; then
-		echo "${path}${sub}/${ssub}"
+		echo "       ${sub}/${ssub}"
 	    fi
 	fi
     done
