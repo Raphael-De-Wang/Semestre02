fa6fadf96735ac9d58895a9df845c8c61cdbfd1c
WANG Jinxin <wangjinxin.java@gmail.com>
2015-07-13 17:17:35 +0200

    Bim Projet Summer

--- BimProjet/CAIJava/CAIJava_pipeline/gen_refseq_search_keys.py
+++ BimProjet/CAIJava/CAIJava_pipeline/gen_refseq_search_keys.py
@@ -1,12 +1,14 @@
 #!env python
 
-tgt = '/tmp/33'
+tgt = '/tmp/txt'
 
 handle = open(tgt)
 
 refseq_list = []
 
 for line in handle:
-    refseq_list.append(line.split()[3])
+    # refseq_list.append(line.split()[3])
+    print line.split()[3]
 
-print ' OR '.join(refseq_list)
+# print ' OR '.join(refseq_list)
+
